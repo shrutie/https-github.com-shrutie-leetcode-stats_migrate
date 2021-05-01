7ecedbdac647567a7b51c56c720c74e00a3de61e
Shruti Burman <bshruti509@gmail.com>
2021-05-01 22:27:47 +0530

    Time: 6 ms (7.29%), Space: 39.3 MB (6.28%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,25 +1,27 @@
 class Solution {
-    //????
-     private static final List<String> letters = IntStream.range(1, 27).mapToObj(Integer::toString).collect(Collectors.toList());
+    //flexing streams
+     private static final List<String> letters = IntStream.range(1, 27)
+                                            .mapToObj(Integer::toString)
+                                            .collect(Collectors.toList());
     int[] memo;
     public int numDecodings(String s) {
         memo = new int[s.length()];
-        Arrays.fill(memo, -1);
-       return dfs(s,0);
+        Arrays.fill(memo, -101);
+       return findWays(s,0);
     }
     
-    public int dfs(String s, int i){
+    public int findWays(String s, int i){
         if(i==s.length())
             return 1;
         
-        if(memo[i]!=-1)
+        if(memo[i]!=-101)
             return memo[i];
         
         int ways=0;
         String  remStr = s.substring(i);
         for(String alpha: letters){
             if(remStr.startsWith(alpha))
-                ways+= dfs(s, i+alpha.length());
+                ways+= findWays(s, i+alpha.length());
         }
         memo[i]=ways;
         return memo[i];
