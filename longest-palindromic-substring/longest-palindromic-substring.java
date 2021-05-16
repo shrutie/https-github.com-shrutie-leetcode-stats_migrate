96c48e32b06feda9cbd8d701edbbeef29fdc3ee3
Shruti Burman <bshruti509@gmail.com>
2021-05-16 16:56:23 +0530

    Time: 241 ms (26.66%), Space: 42.4 MB (29.80%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,27 +1,22 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s.length() <=1)
-            return s;
-        
         int n = s.length();
+        if(s.length() ==1){
+            return s;
+        }
         boolean[][] isPall = new boolean[n][n];
-        int startIdx =0;
         int maxm =0;
-        
-        for(int i=n-1;i>=0;i--){
+        int start =0;
+        for(int i = n-1;i>=0;i--){
             for(int j=i;j<n;j++){
-                isPall[i][j] = (s.charAt(i) == s.charAt(j)) &&
-                                ( (j-i <3) || 
-                                 isPall[i+1][j-1]
-                                );
-            if(isPall[i][j] && j-i+1 > maxm){
-                startIdx = i;
-                maxm = j-i+1;
-            }
+                isPall[i][j] = (s.charAt(i) == s.charAt(j) &&
+                              (j-i<3 || isPall[i+1][j-1] ));
+                if(j-i+1 >maxm && isPall[i][j]){
+                    maxm = j-i+1;
+                    start =i;
+                }
             }
         }
-        
-        return s.substring(startIdx, startIdx+maxm);
-        
+        return s.substring(start, start+maxm);
     }
 }
